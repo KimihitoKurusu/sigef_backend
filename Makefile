3e67025fc2d46a4c0abd69a8474bcbf16a9df3dd
leballagas <leballagas@quental.com>
2024-01-09 14:05:34 -0500

    feat(docker): add configuration for version "3" and define services in docker-compose.local.yml
    This commit introduces configuration updates for version "3" and adds service definitions to the docker-compose.yml file. Notable changes include the addition of volume definitions for the PostgreSQL service, adjustments to the web service build context, and the inclusion of pgAdmin with specified environment variables.
    
    Changes:
    - Set version to "3" in the docker-compose.yml file.
    - Define services: postgres, web, and pgadmin.
    - Add volume definitions for postgres_data_local and postgres_backup_local.
    - Update build context for the web service in the Dockerfile.
    - Include pgAdmin service with specified environment variables.

--- Makefile
+++ Makefile
@@ -2,17 +2,42 @@
 
 .PHONY: install run migrate superuser
 
+setup:
+	docker network create sigef_local_network || true
+	docker-compose -f docker-compose.local.yml build
+	docker-compose -f docker-compose.local.yml run --rm web python manage.py migrate
+	docker-compose -f docker-compose.local.yml run --rm web python manage.py createsuperuser
+
+build:
+	docker-compose -f docker-compose.local.yml build
+
 install:
 	pipenv install
 
+down:
+	docker-compose -f docker-compose.local.yml down
 start:
-	pipenv run python manage.py runserver
+	docker-compose -f docker-compose.local.yml up --attach web
+
+make-migrations:
+	docker-compose -f docker-compose.local.yml run --rm web python manage.py makemigrations
+
+merge-migrations:
+	docker-compose -f docker-compose.local.yml run --rm web python manage.py makemigrations --merge
 
 migrate:
-	pipenv run python manage.py migrate
-
-makemigrations:
-	pipenv run python manage.py makemigrations
+	docker-compose -f docker-compose.local.yml run --rm web python manage.py migrate
 
 superuser:
 	pipenv run python manage.py createsuperuser
+clear-volumes:
+	docker-compose -f docker-compose.local.yml down -v
+
+dbshell:
+	docker-compose -f docker-compose.local.yml run --rm web python manage.py dbshell
+
+managepy:
+	docker-compose -f docker-compose.local.yml run --rm web python manage.py $(ARGS)
+
+pgadmin:
+	docker-compose -f docker-compose.local.yml up --attach pgadmin
--- sigef_backend/settings.py
+++ sigef_backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-yedogu7l73s7-tk=9ok=*t^8kt9#%4m=5r8!54$3)1xvl(kv6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 # Application definition
 
@@ -69,17 +67,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sigef_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+from decouple import config
+
+# Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT', default='', cast=int),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -112,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
