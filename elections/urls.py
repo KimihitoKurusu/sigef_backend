02f411c6c65ff8184e6d047f98ce26081b4a25b5
luiso96ballagas <luiso96ballagas@gmail.com>
2024-01-18 10:48:31 -0500

    feat(elector-registry): add vote url and action

--- elections/urls.py
+++ elections/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, Route
 from .views import *
+# Crea una nueva clase de router que extienda DefaultRouter
+class CustomRouter(DefaultRouter):
+    def get_routes(self, viewset):
+        """
+        Añade una ruta adicional para el método personalizado 'vote'.
+        """
+        routes = super().get_routes(viewset)
+        custom_routes = [
+            # Definir la ruta para el método 'vote'
+            Route(
+                url=r'^{prefix}/{lookup}{trailing_slash}vote/$',
+                mapping={'post': 'vote'},
+                name='{basename}-vote',
+                detail=True,
+                initkwargs={'suffix': 'Vote'}
+            ),
+        ]
+        return routes + custom_routes
 
-router = DefaultRouter()
+# Crea una instancia de tu nuevo router personalizado
+router = CustomRouter()
+
 router.register(r'institutions', InstitutionViewSet)
 router.register(r'campuses', CampusViewSet)
 router.register(r'faculties', FacultyViewSet)
